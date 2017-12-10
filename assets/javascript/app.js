bc0bded3edab0428b14151874b61a73c47ede438
Ben French <ben.jminw@gmail.com>
2017-12-10 15:39:19 -0700

    Some HTMl and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+var triviaGame = {
+    questionList: [{
+        question: "Question 1", answers: [{ ans1: "text", isCorrect: true },
+        { ans2: "text", isCorrect: false },
+        { ans2: "text", isCorrect: false },
+        { ans2: "text", isCorrect: false }]
+    },
+    {
+        question: "Question 2",
+        answers: [
+            { ans1: "text", isCorrect: true },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false }]
+    },
+    {
+        question: "Question 3",
+        answers: [
+            { ans1: "text", isCorrect: true },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false }]
+    },
+    {
+        question: "Question 4",
+        answers: [
+            { ans1: "text", isCorrect: true },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false }]
+    },
+    {
+        question: "Question 5",
+        answers: [
+            { ans1: "text", isCorrect: true },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false },
+            { ans2: "text", isCorrect: false }]
+    },],
+
+    startgame: {
+        // start the game
+    },
+
+
+}
